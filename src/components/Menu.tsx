9bf771062411ee5a56844571de9b1bd55f2cd82d
Vitor Hugo <vitor4oliveira@gmail.com>
2022-09-26 20:36:14 -0300

    Adicionando Componente Menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,33 +1,66 @@
-//O menu era ter dois formatos, um especifico para Home e outro para as demais páginas
-//Tudo isso será controlado pela props modo, que será enviada por quem estiver chamando o Menu
-//Modo terá dois valores: default e home
-
 import Link from "next/link";
 import Image from "next/image"
 import logo from '../../public/images/home/logo.png'
-import styles from '../styles/Home.module.css'
-import { iconePesquisa } from "./icons";
+import styles from '../styles/Menu.module.css'
+import { iconePesquisa, iconeMenuMobile } from "./icons";
+import { useEffect, useState } from "react";
+
 
 export default function Menu(props) {
+    const [menuMobile, setMenuMobile] = useState(styles.desabilitar)
+    const [pesquisa, setPesquisa] = useState('')
+
+
+    function ativarMenuMobile() {
+        console.log(styles)
+        menuMobile === styles.habilitar ? setMenuMobile(styles.desabilitar) : setMenuMobile(styles.habilitar)
+    }
+
     return (
-        <header className={styles.container}>
-            <div className={styles.image}>
-                <Image src={logo} alt="Logo do site, nele temos as palvras Plataforma de Estágio" />
+        <>
+            <header className={styles.container}>
+                <div className={styles.image}>
+                    <Image src={logo} alt="Logo do site, nele temos as palvras Plataforma de Estágio" />
+                </div>
+                <div className={`${styles.botaoMenuMobile}`}>
+                    <button onClick={ativarMenuMobile}>{iconeMenuMobile}</button>
+                </div>
+                <div className={`${styles.menuDesktop}`}>
+                    <nav className={styles.container__menu}>
+                        <ul className={styles.menu}>
+                            <li className={styles.menuItems}><Link href="./">Home</Link></li>
+                            <li className={styles.menuItems}><Link href="./vagas">Vagas</Link></li>
+                            <li className={styles.menuItems}><Link href="./contato">Contato</Link></li>
+                            <li className={styles.menuItems}><Link href="./sobrenos">Sobre Nós</Link></li>
+                        </ul>
+                    </nav>
+                    <div className={styles.containerPesquisa}>
+                        <input type="text" value={pesquisa} onChange={e => setPesquisa(e.target.value)} className={styles.inputPesquisa} placeholder="Digite para pesquisar..." />
+                        <button className={styles.botaoPesquisa}>{iconePesquisa}</button>
+                    </div>
+                    <div className={styles.containerCadastro}>
+                        <button className={styles.botaoLogin}>Login</button>
+                        <button className={styles.botaoCadastro}>Cadastro</button>
+                    </div>
+                </div>
+            </header>
+            <div className={`${styles.menuMobile} ${menuMobile}`}>
+                <div className={styles.containerMobilePesquisa}>
+                    <input type="text" value={pesquisa} onChange={e => setPesquisa(e.target.value)} className={styles.inputMobilePesquisa} placeholder="Digite para pesquisar..." />
+                    <button className={styles.botaoMobilePesquisa}>{iconePesquisa}</button>
+                </div>
+                <ul className={styles.menuMobileList}>
+                    <li className={styles.menuMobileItems}><Link href="./">Home</Link></li>
+                    <li className={styles.menuMobileItems}><Link href="./vagas">Vagas</Link></li>
+                    <li className={styles.menuMobileItems}><Link href="./contato">Contato</Link></li>
+                    <li className={styles.menuMobileItems}><Link href="./sobrenos">Sobre Nós</Link></li>
+                </ul>
+                <div className={styles.containerMobileCadastro}>
+                    <button className={styles.botaoMobileLogin}>Login</button>
+                    <button className={styles.botaoMobileCadastro}>Cadastro</button>
+                </div>
             </div>
-            <ul className={styles.menu}>
-                <li className={styles.menuItems}><Link href="./vagas">Vagas</Link></li>
-                <li className={styles.menuItems}><Link href="./contato">Contato</Link></li>
-                <li className={styles.menuItems}><Link href="./sobrenos">Sobre Nós</Link></li>
-                <li className={styles.menuItems}><Link href="./termos">Termos de uso</Link></li>
-            </ul>
-            <div className={styles.containerBotao}>
-                <input type="text" className={styles.inputPesquisa} placeholder="Digite para pesquisar..."/>
-                <button className={styles.botaoPesquisa}>{iconePesquisa}</button>
-            </div>
-            <div>
-                <button>Login</button>
-                <button>Cadastro</button>
-            </div>
-        </header>
+        </>
     )
 }
+
--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -3,3 +3,9 @@ export const iconePesquisa = (
         <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
     </svg>
 )
+
+export const iconeMenuMobile = (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+    </svg>
+)
