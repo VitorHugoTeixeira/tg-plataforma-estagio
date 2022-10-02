7e3ef8f9f41aeaf828b130c86c285ffea208b827
Vitor Hugo <vitor4oliveira@gmail.com>
2022-10-02 01:18:56 -0300

    Removendo campo de pesquisa do Menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,14 +2,12 @@ import Link from "next/link";
 import Image from "next/image"
 import logo from '../../public/images/home/logo.png'
 import styles from '../styles/Menu.module.css'
-import { iconePesquisa, iconeMenuMobile } from "./icons";
+import { iconeMenuMobile } from "./icons";
 import { useEffect, useState } from "react";
 
 
 export default function Menu(props) {
     const [menuMobile, setMenuMobile] = useState(styles.desabilitar)
-    const [pesquisa, setPesquisa] = useState('')
-
 
     function ativarMenuMobile() {
         console.log(styles)
@@ -34,10 +32,6 @@ export default function Menu(props) {
                             <li className={styles.menuItems}><Link href="./sobrenos">Sobre Nós</Link></li>
                         </ul>
                     </nav>
-                    <div className={styles.containerPesquisa}>
-                        <input type="text" value={pesquisa} onChange={e => setPesquisa(e.target.value)} className={styles.inputPesquisa} placeholder="Digite para pesquisar..." />
-                        <button className={styles.botaoPesquisa}>{iconePesquisa}</button>
-                    </div>
                     <div className={styles.containerCadastro}>
                         <button className={styles.botaoLogin}>Login</button>
                         <button className={styles.botaoCadastro}>Cadastro</button>
@@ -45,10 +39,6 @@ export default function Menu(props) {
                 </div>
             </header>
             <div className={`${styles.menuMobile} ${menuMobile}`}>
-                <div className={styles.containerMobilePesquisa}>
-                    <input type="text" value={pesquisa} onChange={e => setPesquisa(e.target.value)} className={styles.inputMobilePesquisa} placeholder="Digite para pesquisar..." />
-                    <button className={styles.botaoMobilePesquisa}>{iconePesquisa}</button>
-                </div>
                 <ul className={styles.menuMobileList}>
                     <li className={styles.menuMobileItems}><Link href="./">Home</Link></li>
                     <li className={styles.menuMobileItems}><Link href="./vagas">Vagas</Link></li>
